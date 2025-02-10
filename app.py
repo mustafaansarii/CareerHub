f3adf3aa9fc45e3f619b9acb4a2a43bd14693df7
Mustafa Ansari <mustafaa.edu@gmail.com>
2025-02-10 18:22:20 +0530

    c

--- app.py
+++ app.py
@@ -260,6 +260,7 @@ def auth_callback():
         flash("Google login failed. Please try again.", "danger")
         return redirect(url_for("login"))
 
+
 if __name__ == "__main__":
   
     app.run(debug=True)
